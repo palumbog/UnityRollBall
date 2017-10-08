27d150f043ba4339759c6d83ce18df29d8b39e39
giuseppe palumbo <pepomast21@hotmail.it>
2017-10-08 16:16:18 +0200

    Added cubes

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+    public float xRotation, yRotation, zRotation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 rotation = new Vector3(xRotation, yRotation, zRotation) * Time.deltaTime;
+        transform.Rotate(rotation);
+	}
+}
